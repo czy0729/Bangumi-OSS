8cf29918e6daae1dd693a1680588b99871ca591d
czy0729 <402731062@qq.com>
2022-01-03 01:20:19 +0800

    20220102

--- avatar.js
+++ avatar.js
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2020-01-17 21:10:52
  * @Last Modified by: czy0729
- * @Last Modified time: 2021-11-04 09:17:36
+ * @Last Modified time: 2022-01-03 01:09:43
  */
 const fs = require('fs')
 const path = require('path')
@@ -10,7 +10,7 @@ const join = require('path').join
 const http = require('http')
 const utils = require('./utils/utils')
 
-const useCache = false
+const useCache = true
 const rewrite = false
 
 const quality = 'l'
